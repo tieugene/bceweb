362a710f8e270771cef1a6f222928dfe63fd79f4
Eugene <ti.eugene@gmail.com>
2022-01-20 23:31:13 +0300

    TXs list

--- bceweb/routes.py
+++ bceweb/routes.py
@@ -12,6 +12,8 @@ Q_DATES_COUNT = "SELECT COUNT(DISTINCT DATE(datime)) FROM bk;"
 Q_DATES = "SELECT DISTINCT DATE(datime) AS date, COUNT(*) AS num FROM bk GROUP BY date ORDER BY date OFFSET {offset} LIMIT {limit};"
 Q_BLOCKS_COUNT = "SELECT COUNT(*) FROM bk WHERE DATE(datime) = '{date}';"
 Q_BLOCKS = "SELECT id, datime, (SELECT COUNT(*) FROM tx WHERE tx.b_id = bk.id GROUP BY bk.id) FROM bk WHERE DATE(datime) = '{date}' ORDER BY id OFFSET {offset} LIMIT {limit};"
+Q_TXS_COUNT = "SELECT COUNT(*) FROM tx WHERE b_id = {bk};"
+Q_TXS = "SELECT id, b_id, hash, (SELECT COUNT(*) FROM vout WHERE vout.t_id_in = tx.id), (SELECT COUNT(*) FROM vout WHERE vout.t_id = tx.id), (SELECT SUM(money) FROM vout WHERE vout.t_id = tx.id) FROM tx WHERE b_id = {bk} ORDER BY id OFFSET {offset} LIMIT {limit};"
 
 bp = Blueprint('bceweb', __name__)
 
@@ -54,15 +56,18 @@ def src_bks(d: str):
     return render_template('src_blocks.html', data=cur, pager=(page, pages))
 
 
-'''
 @bp.route('/src/bk/<int:bk>', methods=['GET'])
-def src_txs():
+def src_txs(bk: int):
     """List txs of block"""
-    ...
+    pages = math.ceil(get_count(Q_TXS_COUNT.format(bk=bk)) / PAGE_SIZE)
+    if (page := request.args.get('page', 1, type=int)) > pages:
+        page = pages
+    cur = vars.CONN.cursor()
+    cur.execute(Q_TXS.format(bk=bk, limit=PAGE_SIZE, offset=(page-1) * PAGE_SIZE))
+    return render_template('src_txs.html', data=cur, pager=(page, pages))
 
 
 @bp.route('/src/tx/<int:tx>', methods=['GET'])
-def src_bks():
+def src_vins(tx: int):
     """List vins and vouts of tx"""
     ...
-'''
